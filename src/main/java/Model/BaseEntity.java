134ca197389e13020595164ba205aabe02a3b224
ma-abbasi78 <maryamabbasii78@gmail.com>
2024-12-05 22:18:21 +0330

    new feature add base entity model

---
+++ src/main/java/Model/BaseEntity.java
@@ -0,0 +1,22 @@
+package Model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass
+public class BaseEntity<ID extends Serializable> implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
